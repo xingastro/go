0c367a9a6d8b499448d375b37b187e5e42e50f18
mpb <xiaomu1024@gmail.com>
2019-09-20 15:03:55 +0800

    method_interface.go

--- mess/method_interface.go
+++ mess/method_interface.go
@@ -15,6 +15,15 @@ func (u *user) notify() {
 	fmt.Println("Sending user email to", u.name, u.email)
 }
 
+type admin struct {
+	name string
+	email string
+}
+
+func (a admin) notify() {
+	fmt.Println("Sending admin email to", a.name, a.email)
+}
+
 func sendNotification(n notifier) {
 	n.notify()
 }
@@ -22,5 +31,17 @@ func sendNotification(n notifier) {
 func main() {
 	u := user{"Bill", "bill@email.com"}
 	u.notify()
-	sendNotification(&u)
+	u1 := &user{"Adam", "Adam@email.com"}
+	u1.notify()
+
+	a := admin{"Bill", "bill@email.com"}
+	a.notify()
+	a2 := &admin{"Adam", "Adam@email.com"}
+	a2.notify()
+
+	var iu notifier = &user{"Bill", "bill@email.com"}
+	iu.notify()
+
+	var ia notifier = admin{"Bill", "bill@email.com"}
+	ia.notify()
 }
